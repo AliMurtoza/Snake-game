6d40c07ff6fa38e23f73fa6e23957faff43ad038
Ali Murtoza Shihab <67653116+AliMurtoza@users.noreply.github.com>
2023-04-21 15:11:48 +0600

    build: add functions and keyboard bindings

--- snake.py
+++ snake.py
@@ -45,5 +45,40 @@ sc.hideturtle()
 sc.goto(0, 260)
 sc.write("Score: 0  High score: 0", align="center", font=("ds-digital", 24, "normal"))
 
-      
+#functions
+def go_up():
+    if head.direction != "down":
+        head.direction = "up"
+def go_down():
+    if head.direction != "up":
+        head.directin = "down"
+def go_left():
+    if head.direction != "right":
+        head.direction = "left"
+def go_right():
+    if head.direction != "left":
+        head.direction = "roght"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y+20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y-20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x-20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x+20)
+
+
+#keyboard bindings
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_left, "a")
+wn.onkeypress(go_right, "d")
+
 
